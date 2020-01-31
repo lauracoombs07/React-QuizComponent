4f1e4de6ea6bf914e8efaf4f47d9b6676e61ed7e
Laura Coombs <laura.coombs07@gmail.com>
2020-01-31 11:28:55 -0700

    quiz rendering in app

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 
+
 let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
@@ -10,7 +11,8 @@ class Quiz extends Component {
 }
 render() {
     return (
-                <div>Quiz</div>
+                <div><div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
+        </div>
             )
         }
     }
